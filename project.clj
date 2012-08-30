d019a2e328a4b0ebe65c5b52bd14ccebafd8e60c
Nelson Morris <nmorris@nelsonmorris.net>
2012-08-29 19:45:08 -0500

    Prepare for development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pedantic "0.0.1"
+(defproject pedantic "0.0.2-SNAPSHOT"
   :description "Checkout out dependency graphs for common unexpected cases"
   :url "https://github.com/xeqi/pedantic"
   :license {:name "Eclipse Public License"
