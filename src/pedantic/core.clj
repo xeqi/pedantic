0137cbb20d763d313edd36eadf1fbdff04e5195f
Nelson Morris <nmorris@nelsonmorris.net>
2012-08-29 21:33:30 -0500

    Show hierarchy and clip subtrees

--- src/pedantic/core.clj
+++ src/pedantic/core.clj
@@ -27,26 +27,46 @@
       (and (higher? real-v expected-v) top-level)))
 
 
-(defn overrulled? [real-dep [parent coords :as individual]]
+(defn overrulled? [real-dep [parent depmap :as individual]]
   (some #(and (not= real-dep %)
               (similiar real-dep %)
               (breaks-expectation? real-dep % (= parent %))
-              %) coords))
-
-(defn overrulled-versions? [collective-deps individual-deps]
-  (filter identity
-          (for [real-dep collective-deps
-                individual individual-deps]
-            (if-let [unexpected (overrulled? real-dep individual)]
-              (vector real-dep (let [parent (first individual)]
-                                 (if-not (= parent unexpected)
-                                   parent)) unexpected)))))
+              %) (keys depmap)))
 
 (defn find-parent [node collective-deps]
   (first (filter identity (for [[dep transative-deps] collective-deps]
                             (if (contains? transative-deps node)
                               dep)))))
 
+(defn find-parents [node collective-deps]
+  (loop [node node
+         children '()]
+    (if-let [parent (first (filter identity
+                                   (for [[dep transative-deps] collective-deps]
+                                     (if (contains? transative-deps node)
+                                       dep))))]
+      (recur parent (conj children node))
+      (conj children node))))
+
+(defn overrulled-versions? [collective-deps individual-deps]
+  (filter identity
+          (for [real-dep collective-deps
+                individual individual-deps]
+                (if-let [unexpected (overrulled? real-dep individual)]
+                  (vector real-dep (find-parents unexpected (second individual)))))))
+
+(defn sublist? [l1 l2]
+  (= (take (count l1) l2) l1))
+
+(defn is-higher-override? [[o e :as d1] [o2 e2 :as d2]]
+  (and (not= d1 d2)
+       (sublist? o o2)
+       (sublist? e e2)))
+
+(defn remove-subtrees [overrides]
+  (remove #(some (fn [x] (is-higher-override? x %)) overrides) overrides))
+
 (defn determine-overrulled [collective-deps individual-deps]
-  (for [[v a b]  (overrulled-versions? (keys collective-deps) individual-deps)]
-    (vector (vector (find-parent v collective-deps) v) (vector a b))))
+  (remove-subtrees
+   (for [[real over]  (overrulled-versions? (keys collective-deps) individual-deps)]
+     (vector (find-parents real collective-deps) over))))
--- test/pedantic/core_test.clj
+++ test/pedantic/core_test.clj
@@ -6,20 +6,24 @@
 (def repo
   '{[a "1"] []
     [a "2"] []
+    [a "3"] []
     [b "1"] [[a "1"]]
     [c "1"] [[a "2"]]
-    [d "1"] [[a "[2]"]]})
+    [c "2"] [[a "3"]]
+    [d "1"] [[a "[2]"]]
+    [e "1"] [[c "1"]]
+    [f "1"] [[c "2"]]})
 
 (use-fixtures :once (add-repo repo))
 (use-fixtures :each clear-tmp)
 
 
 (defn map-to-deps [coords]
-  (into {} (map #(vector % (keys (resolve-deps [%]))) coords)))
+  (into {} (map #(vector % (resolve-deps [%])) coords)))
 
 (deftest top-level-soft-vs-transative-one-layer-soft
   (let [deps '[[a "1"] [c "1"]]]
-    (is (= '[[[nil [a "1"]]
+    (is (= '[[[[a "1"]]
               [[c "1"] [a "2"]]]]
            (determine-overrulled (resolve-deps deps)
                                  (map-to-deps deps))))))
@@ -40,6 +44,31 @@
 (deftest top-level-soft-vs-transative-hard
   (let [deps '[[a "1"] [d "1"]]]
     (is (= '[[[[d "1"] [a "2"]]
-              [nil [a "1"]]]]
+              [[a "1"]]]]
            (determine-overrulled (resolve-deps deps)
                                  (map-to-deps deps))))))
+
+(deftest show-whole-parent-heirarchy
+  (let [deps '[[e "1"] [a "1"]]]
+    (is (= '[[[[a "1"]]
+              [[e "1"] [c "1"] [a "2"]]]]
+           (determine-overrulled (resolve-deps deps)
+                                 (map-to-deps deps))))))
+
+(deftest clip-subtrees
+  (let [deps '[[e "1"] [f "1"]]]
+    (is (= '[[[[e "1"] [c "1"]] [[f "1"] [c "2"]]]]
+           (determine-overrulled (resolve-deps deps)
+                                 (map-to-deps deps))))))
+
+(comment
+  (let [deps '[[org.clojure/clojure "1.4.0"]
+               [com.cemerick/friend "0.0.9"]
+               [noir "1.3.0-beta9"]]
+        resolve-deps (fn [x] (cemerick.pomegranate.aether/resolve-dependencies
+                             :coordinates x
+                             :repositories (merge cemerick.pomegranate.aether/maven-central
+                                                  {"clojars" "http://clojars.org/repo"})))
+        map-to-deps (fn [coords] (into {} (map #(vector % (resolve-deps [%])) coords)))]
+    (determine-overrulled (resolve-deps deps)
+                          (map-to-deps deps))))
