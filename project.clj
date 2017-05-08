bd2c9e4ed15832dd83ac43df91ac2bdce3258724
Phil Hagelberg <phil@hagelb.org>
2017-05-08 16:35:00 -0700

    Aether is under org.eclipse now, not org.sonatype.
    Well, OK, Aether is gone completely, but Apache Maven Resolver (which
    is its replacement) hasn't been moved out of the org.eclipse package
    yet, only out of the corresponding group-id.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.cemerick/pomegranate "0.2.0"]]
+                 [com.cemerick/pomegranate "0.4.0-SNAPSHOT"]]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :aliases {"all" ["with-profile" "test:test,1.5:test,1.6"]})
